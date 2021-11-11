0fa7af9806f305e405ad994bc681ff19efd4da32
Julliette Huynh <isakurakitsune@gmail.com>
2021-11-10 20:35:23 -0600

    Comparison Operators

---
+++ 2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,34 @@
+// create a constant variable (const)
+const bunny = "Chocolate";
+
+// create a variable that can be reassigned (let)
+let wd40;
+wd40 = "house";
+
+// create three variables and assign them values of different data types (=)
+let gee = "sleep"; //string
+let age = 5; //number
+let pretty = true //boolean
+
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof pretty);
+
+
+// change the value referenced by a variable (dynamic typing)
+let variable1 = false;
+variable1 = 24; //this changes the typeof into a number because we reassigned it.
+
+
+// print the type of the variable you just changed (typeof)
+
+// create variables and give them values to complete the sentences that will print
+// ? which variables need to be created?
+// ? what type of data needs to go in each variable?
+
+let name = "Tom";
+let color = "blue";
+let number = 5;
+let sunny = false;
+
+console.log("My name is " + name  + ", my eyes are " + color + ".  My fave number is " + number + ".")
---
+++ Section02/2.3.3-ComparisonOperators.js
@@ -0,0 +1,33 @@
+// ? log only the expressions that evaluate to true
+// console.log(14 > 8);
+// console.log(42 > 44);
+// console.log(1 > 1);
+
+// console.log(31 < 22);
+// console.log(9 < 13);
+// console.log(0 < 0);
+
+// 12 >= 12.5;
+// 60 >= 40;
+// 2 >= 2;
+
+// 12 <= 12.5;
+// 14 <= 0.3;
+// 5 <= 5;
+
+// 3 === 3;
+// 3 === "3";
+// "3" === "3";
+// 3 === 4;
+
+// 2 !== "2";
+// "2" !== "2"; 
+// 2 !== 1;
+
+"two" == 2;
+2 == "2"; //turns out as true
+2 == 2; // turns out true
+
+3 != "three"; //false
+"3" != 3; //false
+3 != 3;
