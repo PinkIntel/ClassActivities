eb3089ae28334223bf96743544aea9248a14536d
Julliette Huynh <isakurakitsune@gmail.com>
2021-11-11 19:48:39 -0600

    WhileLoops Activity

---
+++ 2.3.7-WhileLoops.js
@@ -0,0 +1,11 @@
+// ? TO DO: create a while loop that TOTALS the numbers from 1 to 100
+// create a variable to represent the current number
+let num = 100;
+// create a variable to represent the current total
+let total = 0;
+// write a while loop that sums the numbers from 1 to 100
+let x = 1;
+while (x <= num) {
+  x++;
+  console.log(total);
+}
